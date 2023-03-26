7b3daf9d9f7226b85c086c48d80c95fa807b7124
sivasaisuryakowshik <sivasaisuryakowshik@gmail.com>
2023-03-26 11:52:02 +0530

    day2

--- src/dayone.java
+++ src/dayone.java
@@ -1,14 +1,16 @@
-import java .util.Scanner;
-
+import java.util.Scanner;
 class hello{
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        char c = sc.next().charAt(0);
-        if (c == 'A' || c == 'a' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
-            System.out.println("it is a vowel");
-        else if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
-            System.out.println("it is a constant");
-        else
-            System.out.println("invalid input");
+    public static void main(String args[])
+    {
+            Scanner sc = new Scanner(System.in);
+            char c = sc.next().charAt(0);
+            if (c>=65&&c<=90||c>=97&&c<=122)
+                System.out.println("it is alphabet");
+            else
+             System.out.println("it is not a alphabet");
+
+
+
+
     }
 }
