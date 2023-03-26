1d0309d5e39a39d440a3cc52f45fd741e3952f7a
sivasaisuryakowshik <sivasaisuryakowshik@gmail.com>
2023-03-26 11:56:32 +0530

    day2

---
+++ src/day2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+    class day2{
+        public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            char c = sc.next().charAt(0);
+            if (c>=65&&c<=90||c>=97&&c<=122)
+                System.out.println("it is alphabet");
+            else
+                System.out.println("it is not a alphabet");
+
+
+
+
+        }
+    }
+
